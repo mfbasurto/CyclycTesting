9f0b071c75da2a762e3cbf111d8d24b3e4b5f115
mfbasurto <107126904+mfbasurto@users.noreply.github.com>
2024-12-23 13:58:34 -0600

    Notes
    links from work laptop

---
+++ Notes.ino
@@ -0,0 +1,12 @@
+//Notes - link to stall guard features
+
+https://www.reddit.com/r/arduino/comments/17gbrgi/finally_got_the_sensorless_homing_to_work_with/?rdt=43559
+https://www.analog.com/media/en/technical-documentation/data-sheets/TMC2209_datasheet_rev1.09.pdf
+file:///C:/Users/mfbas/Downloads/Stepper%20Driver%20-%20TMC2209_SilentStepStick_Rev110.pdf
+https://learn.watterott.com/silentstepstick/pinconfig/tmc2209/
+https://github.com/KushagraK7/TMC2209_sensorless_homing_test/blob/main/TMC_AccelStepper_Sensorless_Homing.ino
+
+
+//George Motor
+https://catalog.orientalmotor.com/item/2-phase-unipolar-stepper-motors/shop-pkp-series-2-phase-unipolar-stepper-motors/pkp223mu09b
+
